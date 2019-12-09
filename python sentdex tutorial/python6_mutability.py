4c9dc6578026b7724abb1b6ed556cd559886c15a
rockingdeep <deeppiyushvora@gmail.com>
2019-12-10 01:28:03 +0530

    adding mutability

---
+++ python sentdex tutorial/python6_mutability.py
@@ -0,0 +1,37 @@
+#learning mutability
+
+x = 1
+def test():
+    x = 2
+test()
+print(x)
+
+
+x = 1
+def test():
+    global x
+    x = 2
+test()
+print(x)
+
+
+x = [1]
+def test():
+    x = [2]
+test()
+print(x)
+
+
+x = [1]
+def test():
+    global x
+    x = [2]
+test()
+print(x)
+
+
+x = [1]
+def test():
+    x[0] = 2
+test()
+print(x)
